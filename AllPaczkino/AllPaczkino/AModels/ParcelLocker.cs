d7f7026c129f9cf17bc6f741375b6ba682ef0848
Piotr Pyt <p.pyt@onet.eu>
2023-10-08 21:21:53 +0200

    PArcelLockers added

--- AllPaczkino/AllPaczkino/AModels/ParcelLocker.cs
+++ AllPaczkino/AllPaczkino/AModels/ParcelLocker.cs
@@ -8,5 +8,14 @@ namespace AllPaczkino.Models
 {
     public class ParcelLocker
     {
+        public int id { get; set; }
+        public string city { get; set; }
+        public string postal_code { get; set; }
+        public string address { get; set; }
+    }
+
+    public class ParcelLockerList
+    {
+        public List<ParcelLocker> parcel_lockers { get; set; }
     }
 }
