c78afd58269f9f5ad094ed2169d78e0bcd8d272f
Adam Wensierski <117477289+wensierski@users.noreply.github.com>
2023-10-08 11:05:32 +0200

    Add files via upload

---
+++ AllPaczkino/AllPaczkino/Parcels/ParcelLabel.cs
@@ -0,0 +1,13 @@
+﻿using AllPaczkino.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllPaczkino.Labels
+{
+    internal class ParcelLabel : Parcel
+    {
+    }
+}
