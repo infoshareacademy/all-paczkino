71971f4cc2d2cd15fc3f14e9ca3de2dad4938a01
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2023-10-08 13:21:38 +0200

    add parcel status enum and property

--- AllPaczkino/AllPaczkino/AModels/Parcel.cs
+++ AllPaczkino/AllPaczkino/AModels/Parcel.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace AllPaczkino.Models
 {
+    public enum ParcelStatus
+    {
+        Registered,
+        Sent,
+        CollectedByCourier,
+        InDelivery,
+        ReadyToCollection,
+        Received,
+        Returned,
+        Lost
+    }
     public class Parcel
     {
+        public ParcelStatus parcelStatus  { get; set; }
     }
 }
