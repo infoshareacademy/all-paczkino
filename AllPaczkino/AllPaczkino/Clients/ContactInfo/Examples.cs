167b460268d91a3518d3d79537c2f0e822c5d146
Zuzia Lobuzia <frydrychzuzanna01@gmail.com>
2023-10-10 17:36:40 +0200

    dodałam zmiany w folderze Client, nowe pliki i dwa usunięte

---
+++ AllPaczkino/AllPaczkino/Clients/ContactInfo/Examples.cs
@@ -0,0 +1,12 @@
+﻿namespace AllPaczkino.Clients.ContactInfo
+{
+    internal static class Examples
+    {
+        public static readonly List<ContactData> DataCollection = new List<ContactData>
+        {
+            new ("Zuzia", "Frydrych", "zuzia.frydrych@op.pl", "601888635", new Adress("Katowicka", 87, "Poznan", "Wlkp", "62-131", null, 10)),
+            new ("Kuba", "Nowak", "kuba.nowak@op.pl", "897395173", new Adress("Polska", 2, "Poznan", "Wlkp", "60-221", null)),
+            new ("Ola", "Wilska", "ola.wilska@op.pl", "607382175", new Adress("Marciniaka", 7, "Poznan", "Wlkp", "60-462", null, 7)),
+        }
+    }
+}
