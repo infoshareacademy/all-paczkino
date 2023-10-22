b71f64e83c952d5223e7180b0a5903e4829c8084
Adam Wensierski <adam.wensierski.24@gmail.com>
2023-10-22 14:03:20 +0200

    nowe dodawanie, usuwanie i modyfikowanie klientow

---
+++ AllPaczkino/AllPaczkino/Repositories/IContactDataRepository.cs
@@ -0,0 +1,20 @@
+﻿using AllPaczkino.Clients.ContactInfo;
+using Parcels.Parcels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllPaczkino.Repositories
+{
+    internal interface IContactDataRepository
+    {
+        IEnumerable<ContactData> GetContactDataList();
+        void AddContact(string name, string surname, string email, string phoneNumber, Adress adress);
+
+        bool DeleteContact(string email);
+
+        bool EditContact(string email, ContactData updatedContact);
+    }
+}
