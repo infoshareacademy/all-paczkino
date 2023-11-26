f8d3c446cf90c2b32edc52208e88dbb42794d476
Adam Wensierski <adam.wensierski.24@gmail.com>
2023-11-26 12:20:06 +0100

    branch with error msb3021

---
+++ AllPaczkino/AllPaczkinoLogic/DAL/Package.cs
@@ -0,0 +1,17 @@
+﻿using AllPaczkino.Clients.ContactInfo;
+using AllPaczkino.Models;
+
+namespace AllPaczkino.DAL
+{
+    public class Package
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public Adress Address { get; set; }
+        public ParcelLocker SenderLocker { get; set; }
+        public ParcelLocker ReceiverLocker { get; set; }
+        public DateTime SendTime { get; set; }
+        public DateTime ReceiveTime { get; set; }
+        public ParcelStatus ParcelStatus { get; set; }
+    }
+}
