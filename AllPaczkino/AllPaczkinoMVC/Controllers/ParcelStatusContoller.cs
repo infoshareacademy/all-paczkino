76efb22312693d93fd3c46eccef442e09062e54a
Patryk Augusewicz <84743481+PatrykAugusewicz@users.noreply.github.com>
2023-12-17 10:46:38 +0000

    URL loading problem

---
+++ AllPaczkino/AllPaczkinoMVC/Controllers/ParcelStatusContoller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllPaczkinoMVC.Controllers
+{
+    public class ParcelStatusContoller : Controller
+    {
+        // GET: ParcelStatus
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
