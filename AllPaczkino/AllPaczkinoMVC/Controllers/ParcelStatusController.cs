1a6264970ca945b28deabdc5ad5ff7b9231341e8
Patryk Augusewicz <84743481+PatrykAugusewicz@users.noreply.github.com>
2024-01-12 20:25:35 +0100

    Visual part completed for Parcel Status Check. Logic and validation needs to be implemented.

--- AllPaczkino/AllPaczkinoMVC/Controllers/ParcelStatusController.cs
+++ AllPaczkino/AllPaczkinoMVC/Controllers/ParcelStatusController.cs
@@ -10,80 +10,95 @@ namespace AllPaczkinoMVC.Controllers
         ParcelsRepository parcelRepository = new();
         List<Parcel> parcelsData;
 
-        // GET: ParcelsControler
+        //GET: ParcelsControler
         public ActionResult Index()
         {
             parcelsData = parcelRepository.GetAll();
 
             return View(parcelsData);
         }
-        // GET: ParcelStatusController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
 
-        // GET: ParcelStatusController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: ParcelStatusController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        // GET: ProductsController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    parcelsData = parcelRepository.GetAll();
 
-        // GET: ParcelStatusController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
 
-        // POST: ParcelStatusController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //    if (parcelsData is null)
+        //    {
+        //        TempData["NotFound"] = "Product not found!";
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(parcelsData);
+        //}
+        //// GET: ParcelStatusController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
 
-        // GET: ParcelStatusController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+        //// GET: ParcelStatusController/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: ParcelStatusController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: ParcelStatusController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: ParcelStatusController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ParcelStatusController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: ParcelStatusController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ParcelStatusController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
