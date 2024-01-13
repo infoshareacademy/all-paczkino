96e387636024435bbc3c07c67fe2810ac87b8f5e
Patryk Augusewicz <84743481+PatrykAugusewicz@users.noreply.github.com>
2024-01-13 18:33:48 +0100

    jeszcze nie dziala logika

--- AllPaczkino/AllPaczkinoMVC/Controllers/ParcelStatusController.cs
+++ AllPaczkino/AllPaczkinoMVC/Controllers/ParcelStatusController.cs
@@ -5,100 +5,139 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AllPaczkinoMVC.Controllers
 {
-    public class ParcelStatusController : Controller
+    using AllPaczkino.Models;
+    using AllPaczkino.Repositories;
+    using Microsoft.AspNetCore.Mvc;
+
+    namespace AllPaczkinoMVC.Controllers
     {
-        ParcelsRepository parcelRepository = new();
-        List<Parcel> parcelsData;
-
-        //GET: ParcelsControler
-        public ActionResult Index()
+        public class ParcelStatusController : Controller
         {
-            parcelsData = parcelRepository.GetAll();
+            private readonly ParcelsRepository parcelRepository;
 
-            return View(parcelsData);
+            public ParcelStatusController()
+            {
+                parcelRepository = new ParcelsRepository();
+
+            }
+
+            // GET: ParcelStatus
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+            // POST: ParcelStatus/Details
+            [HttpPost]
+            public IActionResult Details(Parcel parcel)
+            {
+                if (ModelState.IsValid)
+                {
+                    // Valid input, process the parcel status check
+                    // You can use parcelRepository or other logic here
+                    return RedirectToAction("Index");
+                }
+
+                // Invalid input, return to the form with validation errors
+                return View("Index", parcel);
+            }
         }
-
-
-        // GET: ProductsController/Details/5
-        //public ActionResult Details(int id)
-        //{
-        //    parcelsData = parcelRepository.GetAll();
-
-
-        //    if (parcelsData is null)
-        //    {
-        //        TempData["NotFound"] = "Product not found!";
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(parcelsData);
-        //}
-        //// GET: ParcelStatusController/Details/5
-        //public ActionResult Details(int id)
-        //{
-        //    return View();
-        //}
-
-        //// GET: ParcelStatusController/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: ParcelStatusController/Create
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create(IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: ParcelStatusController/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: ParcelStatusController/Edit/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: ParcelStatusController/Delete/5
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: ParcelStatusController/Delete/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Delete(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
     }
+
+    //public class ParcelStatusController : Controller
+    //{
+    //    ParcelsRepository parcelRepository = new();
+    //List<Parcel> parcelsData;
+
+    //    //GET: ParcelsControler
+    //    public ActionResult Index()
+    //    {
+    //        parcelsData = parcelRepository.GetAll();
+
+    //        return View(parcelsData);
+    //    }
+
+
+    // GET: ProductsController/Details/5
+    //public ActionResult Details(int id)
+    //{
+    //    parcelsData = parcelRepository.GetAll();
+
+
+    //    if (parcelsData is null)
+    //    {
+    //        TempData["NotFound"] = "Product not found!";
+    //        return RedirectToAction(nameof(Index));
+    //    }
+    //    return View(parcelsData);
+    //}
+    //// GET: ParcelStatusController/Details/5
+    //public ActionResult Details(int id)
+    //{
+    //    return View();
+    //}
+
+    //// GET: ParcelStatusController/Create
+    //public ActionResult Create()
+    //{
+    //    return View();
+    //}
+
+    //// POST: ParcelStatusController/Create
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public ActionResult Create(IFormCollection collection)
+    //{
+    //    try
+    //    {
+    //        return RedirectToAction(nameof(Index));
+    //    }
+    //    catch
+    //    {
+    //        return View();
+    //    }
+    //}
+
+    //// GET: ParcelStatusController/Edit/5
+    //public ActionResult Edit(int id)
+    //{
+    //    return View();
+    //}
+
+    //// POST: ParcelStatusController/Edit/5
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public ActionResult Edit(int id, IFormCollection collection)
+    //{
+    //    try
+    //    {
+    //        return RedirectToAction(nameof(Index));
+    //    }
+    //    catch
+    //    {
+    //        return View();
+    //    }
+    //}
+
+    //// GET: ParcelStatusController/Delete/5
+    //public ActionResult Delete(int id)
+    //{
+    //    return View();
+    //}
+
+    //// POST: ParcelStatusController/Delete/5
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public ActionResult Delete(int id, IFormCollection collection)
+    //{
+    //    try
+    //    {
+    //        return RedirectToAction(nameof(Index));
+    //    }
+    //    catch
+    //    {
+    //        return View();
+    //    }
+    //}
 }
+
