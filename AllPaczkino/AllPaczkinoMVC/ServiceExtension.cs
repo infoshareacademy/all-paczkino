f531594369afd7f312694268c496b83c267f42ee
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2024-02-13 16:54:34 +0100

    add servise extension

---
+++ AllPaczkino/AllPaczkinoMVC/ServiceExtension.cs
@@ -0,0 +1,25 @@
+﻿using AllPaczkinoPersistance;
+using Microsoft.AspNetCore.Identity;
+
+namespace AllPaczkinoMVC
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<PaczkinoDbContext>();
+
+            services.ConfigureApplicationCookie(option => {
+                option.LoginPath = "/Identity/Signin";
+                option.AccessDeniedPath = "/Identity/AccessDenied";
+                option.ExpireTimeSpan = TimeSpan.FromHours(10);
+            });
+            services.AddAuthorization();
+
+            services.AddControllersWithViews();
+
+            return services;
+        }
+    }
+}
